95dc0dbea9418309e4985849020824b73bc09b6d
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-05-06 20:17:24 +0300

    Create User Registration and Login Endpoints

--- django_react_project/backend/api/urls.py
+++ django_react_project/backend/api/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from .views import PostList, PostDetail
+from rest_framework.routers import DefaultRouter
+from .views import UserViewSet
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
 ]
+
+router = DefaultRouter()
+router.register(r'users', UserViewSet)
+
+urlpatterns = router.urls
--- django_react_project/backend/api/views.py
+++ django_react_project/backend/api/views.py
@@ -1,6 +1,10 @@
 from rest_framework import generics
 from .models import Post
 from .serializers import PostSerializer
+from .serializers import UserSerializer
+from rest_framework import viewsets
+from rest_framework.permissions import AllowAny
+from django.contrib.auth.models import User
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -9,3 +13,8 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [AllowAny]
