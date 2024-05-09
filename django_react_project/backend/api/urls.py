0bd6f80be6194d8fc871a7a69ca6b1be08cd9c50
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-05-09 18:41:04 +0300

    jwt tocken fixed

--- django_react_project/backend/api/urls.py
+++ django_react_project/backend/api/urls.py
@@ -3,11 +3,14 @@ from .views import PostList, PostDetail
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet
 from .views import login
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('posts/', PostList.as_view(), name='post-list'),
     path('posts/<int:pk>/', PostDetail.as_view(), name='post-detail'),
     path('login/', login),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
 
 router = DefaultRouter()
