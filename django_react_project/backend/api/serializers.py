a36ec6b4d82b3d8cb8907ea5df352a804f0a74c4
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-05-17 19:13:41 +0300

    Create Serializer for Content Model

--- django_react_project/backend/api/serializers.py
+++ django_react_project/backend/api/serializers.py
@@ -1,6 +1,7 @@
 from rest_framework import serializers
 from .models import Post
 from django.contrib.auth.models import User
+from .models import Content
 
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
@@ -11,3 +12,9 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'first_name', 'last_name')
+
+class ContentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Content
+        fields = ['id', 'title', 'body', 'author', 'created_at', 'updated_at']
+        read_only_fields = ['author', 'created_at', 'updated_at']
