da275d753c7552d4e04dc1e835f3e8a9baf22bfc
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-05-21 02:49:36 +0300

    Connect Redux to React Components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,26 @@
-import React, { useState } from 'react';
+import React, { useEffect } from 'react';
 import axios from 'axios';
 import { BrowserRouter as Router, Route, Redirect, Switch } from 'react-router-dom';
+import { useDispatch, useSelector } from 'react-redux';
+import { fetchContents } from './redux/actions/contentActions';
 import Login from './components/Login';
 import Profile from './components/Profile';
 
 function App() {
   const [auth, setAuth] = useState(false);
   const [token, setToken] = useState('');
+  const dispatch = useDispatch();
+  
+  const contents = useSelector(state => state.content.contents);
+  const loading = useSelector(state => state.content.loading);
+  const error = useSelector(state => state.content.error);
 
   const login = (username, password) => {
     axios.post('http://localhost:8000/api/login/', { username, password })
       .then(response => {
         setToken(response.data.access);
         setAuth(true);
+        dispatch(fetchContents());  // Fetch content immediately after login
       })
       .catch(error => console.error('Error logging in:', error));
   };
@@ -21,6 +29,12 @@ function App() {
     setToken('');
     setAuth(false);
   };
+
+  useEffect(() => {
+    if (auth) {
+      dispatch(fetchContents());
+    }
+  }, [auth, dispatch]);
 
   return (
     <Router>
@@ -32,7 +46,17 @@ function App() {
           {auth ? <Redirect to="/profile" /> : <Login login={login} />}
         </Route>
         <Route path="/profile">
-          {auth ? <Profile token={token} setAuth={setAuth} /> : <Redirect to="/login" />}
+          {auth ? (
+            <Profile 
+              token={token} 
+              setAuth={setAuth}
+              contents={contents}
+              loading={loading}
+              error={error}
+            />
+          ) : (
+            <Redirect to="/login" />
+          )}
         </Route>
         <Route path="/logout">
           {handleLogout()}
