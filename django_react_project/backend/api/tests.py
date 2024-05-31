e200b5df89e72bbbe348ede5253b8e0540db68f5
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-05-31 04:39:41 +0300

    Write Tests for the Recommendation API Endpoint

--- django_react_project/backend/api/tests.py
+++ django_react_project/backend/api/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+class RecommendationApiTest(TestCase):
+
+    def test_recommendation_api(self):
+        response = self.client.get(reverse('recommend_content', args=[1]))
+        self.assertEqual(response.status_code, 200)
+        self.assertIn('recommended_content_ids', response.json())
