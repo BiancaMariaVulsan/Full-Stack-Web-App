6231784d59a72bdcbbb1672141f9f466b729fc5e
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-06-03 21:36:01 +0300

    Configure Redis for Django Caching

--- django_react_project/backend/backend/settings.py
+++ django_react_project/backend/backend/settings.py
@@ -193,3 +193,18 @@ ALGOLIA = {
     'API_KEY': 'your_algolia_admin_api_key',
     'INDEX_PREFIX': 'your_prefix_',
 }
+
+# Redis cache settings
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+# Enable cache in session
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "default"
