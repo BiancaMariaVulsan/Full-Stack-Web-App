d1f9f8e26ec0bf168d7d8bd0ee39efce466cffd6
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-06-05 19:38:43 +0300

    Implement Redis Caching in a Django View

--- django_react_project/backend/api/views.py
+++ django_react_project/backend/api/views.py
@@ -15,6 +15,7 @@ from .models import Content
 from .serializers import ContentSerializer
 from algoliasearch_django import raw_search
 from ml.load_model import load_recommendation_model, get_recommendations
+from django.views.decorators.cache import cache_page
 
 class PostList(generics.ListCreateAPIView):
     queryset = Post.objects.all()
@@ -66,3 +67,8 @@ def recommend_content(request, content_id):
     cosine_sim, df, _ = load_recommendation_model()
     recommendations = get_recommendations(int(content_id), cosine_sim, df)
     return Response({'recommended_content_ids': recommendations})
+
+@api_view(['GET'])
+@cache_page(60 * 15)  # Cache the view for 15 minutes
+def cached_view(request):
+    return Response({"message": "This is a cached response"})
