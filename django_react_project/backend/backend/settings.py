87a1bbca52804c8a8947f4158db927c9284dd58d
Vulsan Bianca Maria <biavulsan@yahoo.com>
2024-07-02 21:56:55 +0300

    Configure Celery in Django

--- django_react_project/backend/backend/settings.py
+++ django_react_project/backend/backend/settings.py
@@ -239,3 +239,11 @@ CHANNEL_LAYERS = {
     },
 }
 
+# Celery Configuration Options
+CELERY_BROKER_URL = 'amqp://localhost'
+CELERY_RESULT_BACKEND = 'rpc://'
+CELERY_ACCEPT_CONTENT = ['json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
+CELERY_TIMEZONE = 'UTC'
+
